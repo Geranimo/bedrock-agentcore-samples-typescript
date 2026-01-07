0845ac677dc1d81f581e4c71b2b47cc7e9949f85
Sundar Raghavan <sdraghav@amazon.com>
2026-01-06 18:04:53 -0800

    chore: add placeholder ts file for typecheck

---
+++ placeholder.ts
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Placeholder file for TypeScript configuration validation.
+ * Remove this file once real examples are added.
+ */
+
+export {}
